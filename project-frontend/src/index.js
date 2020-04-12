926626ffe7eaf6b98843539e533b0eb5ce1efb54
rick <github email address>
2020-04-12 16:12:01 -0400

    Added functions to render login form

--- project-frontend/src/index.js
+++ project-frontend/src/index.js
@@ -1,12 +1,29 @@
 console.log("testing...");
 document.addEventListener("DOMContentLoaded", function () {
-  renderLanding()
-  
+	let main = document.getElementById("main");
+	renderLanding();
 });
 function renderLanding() {
-	document.getElementById("main").innerHTML = ` <button id = "login">Login</button> 
+	main.innerHTML = ` <button id = "login">Login</button> 
   <button id ="signup">Signup</button>`;
-  document.querySelectorAll('button').forEach(button => {
-    button.addEventListener('click', (e)=> console.log(e.target.innerText))
-  });
+	document.querySelectorAll("button").forEach((button) => {
+    let action = button.innerText
+		button.addEventListener("click", (e) => {
+      if (action === 'Signup' ) {
+        
+      } else {
+        renderLogin()
+      }
+    });
+	});
+}
+
+function renderLogin() {
+	main.innerHTML = `   <form action="http://localhost:30001/sessions" method="post">
+  <label for="email"></label>
+  <input type="text" name="email" id="email">
+  <label for="password"></label>
+  <input type="text"name = "password" id = 'password'>
+  <input type="submit" value="Login">
+</form>`;
 }
