65a26024435595d4a84d5ed089037fe1a4e9693b
rick <github email address>
2020-04-17 20:03:28 -0400

    Added render method for watchlist

--- project-frontend/javascripts/models/watchlist.js
+++ project-frontend/javascripts/models/watchlist.js
@@ -1,12 +1,19 @@
 class Watchlist {
-    static all = []
-    constructor(data) {
-        this.name = data.name
-        this.stocks = data.stocks
-        this.id = data.id
-        this.save
-    }
-    save(){
-        Watchlist.all.push(this)
-    }
+	static all = [];
+	constructor(data) {
+		this.name = data.name;
+		this.stocks = data.stocks;
+		this.id = data.id;
+		this.save();
+	}
+	save() {
+		Watchlist.all.push(this);
+	}
+	renderdiv() {
+		return `<div id="watchlist-item-${this.id}">
+        <button id = "${this.id}"style="width: 15%;">
+		<h5 id = "h5-${this.id}">${this.name}</h5>
+        </button><br><button class = "delete-${this.id}">delete</button>
+		</div>`;
+	}
 }
