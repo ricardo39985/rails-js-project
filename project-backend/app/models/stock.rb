dcae8c9cb0933069cc1f2973f3bd143a2b5151ed
rick <github email address>
2020-04-19 00:44:03 -0400

    added dependent delete

--- project-backend/app/models/stock.rb
+++ project-backend/app/models/stock.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class Stock < ApplicationRecord
-  has_many :stock_watchlists
+  has_many :stock_watchlists, dependent: :delete_all
   has_many :watchlists, through: :stock_watchlists
 end
--- project-backend/app/models/watchlist.rb
+++ project-backend/app/models/watchlist.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class Watchlist < ApplicationRecord
-  has_many :stock_watchlists
+  has_many :stock_watchlists, dependent: :delete_all
   has_many :stocks, through: :stock_watchlists
 end
