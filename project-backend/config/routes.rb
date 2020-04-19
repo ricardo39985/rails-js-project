86266870a7935c82dca141c63a3ce4af5e10968d
rick <github email address>
2020-04-19 01:09:38 -0400

    Add routing for delete

--- project-backend/config/routes.rb
+++ project-backend/config/routes.rb
@@ -2,6 +2,7 @@
 
 Rails.application.routes.draw do
   resources :watchlists
+  resources :stocks
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/test', to: 'application#test'
   resources :sessions, only: %i[create destroy]
